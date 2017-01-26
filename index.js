df5613707445611e75c983b3d246a5788a2a7e97
Geoffrey Challen <challen@buffalo.edu>
2017-01-26 12:41:49 -0500

    Simple hook listener.

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ var _ = require('underscore'),
     express = require('express');
 
 var config = _.extend(_.clone(defaults), JSON.parse(fs.readFileSync(argv._[0])));
-console.log(config);
+if (config.verbose) {
+  console.log(config);
+}
 
 app = express();
-
 app.post(/.*/, function (req, res) {
   console.log("Here");
 });
-
 app.listen(config.port);
 
 // vim: ts=2:sw=2:et
